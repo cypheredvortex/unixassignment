fe5da31d6f7876a0e9e2e735c2bd3e68a7d30886
cypheredvortex <alimalik21realms@gmail.com>
2024-12-13 11:21:33 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,9 +1,8 @@
-
 function guessinggame(){
     no_of_files=$(pwd | ls | wc -l)
     while true;
     do
-        echo "pleas enter your guess"
+        echo "please enter your guess"
         read  number
         if [ $number -lt $no_of_files ]
         then
@@ -12,7 +11,7 @@ function guessinggame(){
         then
             echo "Your guess is greater than the number of files"
         else
-            echo "Congratulation you have guessed correctly!"
+            echo "Congratulations you have guessed correctly!"
         break;
         fi
     done
