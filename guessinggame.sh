2fd05452b2be852fedbe84e15c62787531bff0f4
cypheredvortex <alimalik21realms@gmail.com>
2024-12-13 11:19:52 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,22 @@
 
+function guessinggame(){
+    no_of_files=$(pwd | ls | wc -l)
+    while true;
+    do
+        echo "pleas enter your guess"
+        read  number
+        if [ $number -lt $no_of_files ]
+        then
+            echo "Your guess is less than the number of files"
+        elif [ $number -gt $no_of_files ]
+        then
+            echo "Your guess is greater than the number of files"
+        else
+            echo "Congratulation you have guessed correctly!"
+        break;
+        fi
+    done
+}
+
+echo "Guess the number of files in your current directory?"
+guessinggame
